010d41ae3532f33ed6fd03ffc40a2a71d8f9d717
Tom-Has <t0m.has@pm.me>
2025-05-24 16:36:21 +0200

    Delete other_content/writeCSV_example.py

--- other_content/writeCSV_example.py
+++
@@ -1,8 +0,0 @@
-file_path = f"claims.csv"
-f = open(file_path, "x")
-f.write("ID,Partner_ID\n")
-
-for claim in claims:
-    f.write(str(claim.id) + "," + str(claim.partner_id.id) + "\n")
-
-f.close()
