76b238eb364f523e92a791838720d798905a8e62
Tom-Has <t0m.has@pm.me>
2025-05-24 16:29:44 +0200

    Delete Sansoft_Odoo_Transfer directory

--- Sansoft_Odoo_Transfer/sale_order_functions.py
+++
@@ -1,9 +0,0 @@
-#obtain prescription files via directory and convert them to Base64 format
-def file_to_base64(row_path):
-    path = "C:\\Users\ThomasHabenschuss\Documents\Datenimports\Schaper\Dokumente\\" + row_path
-    try:
-        with open(path, "rb") as file:
-            return base64.b64encode(file.read()).decode('utf-8')
-    except Exception as e:
-        print(f"Fehler beim Kodieren der Datei {path}: {e}")
-        return None
