7072ab4f4c8bb34827cc9d6ed20d55d46f825fb4
Tom-Has <t0m.has@pm.me>
2025-06-07 18:40:39 +0200

    Update and rename logfile_inspection.py to inspect_log_files.py

---
+++ shell_scripts/inspect_log_files.py
@@ -0,0 +1,23 @@
+import os
+import gzip
+
+def inspect_log_files(search_string_list):
+    for file in os.listdir('/home/odoo/logs'):
+        try:
+            filename = os.fsdecode(file)
+            full_path = os.path.join(log_dir, filename)       
+            counter_list = [0] * len(search_string_list)
+            open_func = gzip.open if filename.endswith(".gz") else open
+            with open_func(full_path, 'rt') as log:
+                for line in log:
+                    for i, search_string in enumerate(search_string_list):
+                        if search_string in line:
+                            counter_list[i] += 1
+            if not all(item == 0 for item in counter_list):
+                print(f"\n{filename} has:")
+                for i, counter in enumerate(counter_list):
+                    if counter > 0:
+                        print(f"{counter} occurrences of '{search_string_list[i]}'.")
+        except:
+            print(f"\n{filename} is not of type .gz or text file.")
+            continue
