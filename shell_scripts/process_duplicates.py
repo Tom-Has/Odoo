9ed39eb41cc000ac14da5d9fd3ef865bd4ce23a3
Tom-Has <t0m.has@pm.me>
2025-08-09 18:19:15 +0200

    Update process_duplicates.py

--- shell_scripts/process_duplicates.py
+++ shell_scripts/process_duplicates.py
@@ -1,14 +1,25 @@
-# Duplikatgruppen SV-Nr. im Deduplizierungsmodell
-id = <..>
-dupe_model = env['data_merge.model'].browse(id)
-dupe_groups = env['data_merge.group'].search([('model_id.id', '=', dupe_model)])
-model_name = dupe_model.res_model_id.name
+# group
+duplicates = env['<model_name>'].read_group(
+    domain=[],
+    fields=['<field_name1>', '<field_name2>', '<field_name3>:<aggregate_func>'],
+    groupby=['<field_name1>', '<field_name2>']
+)
 
-# Iteration ueber Duplikatgruppen:
-for group in sv_dupe_groups:
-    # Iteration uber Eintraege in Gruppe
-    # Funktionaler Code hier
-    
-    for record in group:
-        # Belasse den Eintrag mit Vema-Referenz, sonst mit Beginn Hauptversicherter, sonst beliebigen
-        full_rec = env[model_name].browse(record.id)
+# process group
+for entry in duplicates:
+    if entry['field_name1'] > 1:
+        print(f"{entry['field_name1']}: corresponds to {entry['field_name3_count']}.")
+
+"""
+Important:
+Each field <field_nameX> in groupby must exist in fields.
+The fields-parameter can contain more fields, e. g. for counts, sums, etc.
+The aggregate functions <aggregate_func> are named count, sum, avg, min, max.
+The result is always a list of dictionaries, the keys being named <field_nameX>, <field_nameX>_count, <field_nameX>_sum, etc.
+"""
+
+"""
+include the following statements at the beginning when operating outside of odoo shell
+from odoo import api, SUPERUSER_ID
+env = api.Environment(cr, SUPERUSER_ID, {})
+"""
