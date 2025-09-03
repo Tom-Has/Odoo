e4d5abed7a89d944c320542731eea33706ac321c
Tom-Has <t0m.has@pm.me>
2025-09-03 14:35:59 +0200

    Update and rename search_mapped_filtered.py to orm_api_search_mapped_filtered.py

--- shell_scripts/orm_api_search_mapped_filtered.py
+++ shell_scripts/orm_api_search_mapped_filtered.py
@@ -1,3 +1,5 @@
+# https://www.odoo.com/documentation/18.0/developer/reference/backend/orm.html
+
 # search() to obtain record set (Boolean evaluation)
 record_set = env['res.partner'].search([('field', 'in', value_list)])
 
@@ -9,4 +11,6 @@ values_list = record_set.mapped(lambda r: r.field1 + r.field2)
 filtered_set = record_set.filtered('partner_id.is_company')
 filtered_set = record_set.filtered(lambda r: r.company_id == user.company_id)
 
-# https://www.odoo.com/documentation/18.0/developer/reference/backend/orm.html
+# note that mapped() takes only one argument, for several valaues without the complete record set consider list of dicts or tuples
+result = [{"name": r.name, "partner": r.partner_id.name} for r in record_set]
+pairs = [(r.name, r.partner_id.name) for r in record_set]
