5a5dd0ffe75903e540d1bac87fffcedb8e8e9b0e
Tom-Has <t0m.has@pm.me>
2025-09-24 09:32:56 +0200

    Update elementaries.py

--- Studio/elementaries.py
+++ Studio/elementaries.py
@@ -11,8 +11,14 @@ name="parent_id"
 # name of field (overwrites String value)
 string="Contact Name"
 
-# placeholder(no default value)
+# placeholder (no default value)
 placeholder="Country"
+
+# graphic display of field values
+widget="many2many_tags"
+
+# additional options for graphic display of field values
+options="{'horizontal': true}"
 
 # record filter for one/many2many field connections
 domain="[('is_company', '=', True)]"
@@ -23,7 +29,10 @@ invisible="((is_company and not parent_id) or company_name) and company_name != 
 # read only conditions
 readonly="type == 'contact' and parent_id"
 
-# context
+# conditions for value requirement
+required="type == 'contact'"
+
+# default values for opening or creating a record
 context="{'default_is_company': True, 'show_vat': True, 'default_user_id': user_id}"
 
 """
