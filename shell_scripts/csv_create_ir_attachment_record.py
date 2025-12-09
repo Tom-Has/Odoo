70d6a391e0d8740226bf37c49ac79ab2414ccd47
Tom-Has <t0m.has@pm.me>
2025-12-09 14:46:49 +0100

    Update csv_create_ir_attachment_record.py

--- shell_scripts/csv_create_ir_attachment_record.py
+++ shell_scripts/csv_create_ir_attachment_record.py
@@ -1,16 +1,17 @@
 """
-Run this script via shell to create attachtments from base64 strings
+Run this snippet via shell to create attachtments from base64 strings
 """
 
-#variable arguments
-paths = ["encoded_pdfs_1.csv", "encoded_pdfs_2.csv", "encoded_pdfs_3.csv"]  # add paths as needed
+# files or paths to files
+paths = ["encoded_pdfs.csv"]  # add or adpapt paths as needed
 
-#constants
+# constants
 att_model = env['ir.attachment']
 res_model_string = 'res.model'    # change name as needed
-start = 0
+counter = 0
 limit = 1000
 
+# writing iteration
 for path in paths:
     with open(path, 'r') as file:
         for index, line in enumerate(file):
@@ -20,10 +21,19 @@ for path in paths:
             name = "prefix-" + parts[0]    # adapt name as needed
             filename = name + ".pdf"    # construct file name as needed
             id = env[res_model_string].search([('name', '=', name)]).id # adapt filter as needed
-            att_model.create({"key": parts[0], "datas": parts[1], "name": name, "res_name": filename, "mimetype": "application/pdf", "type": "binary", "res_model": res_model_string, "res_id": id})
-            start += 1
-            if start % limit == 0:
+            att_model.create({
+                "key": parts[0],
+                "datas": parts[1],
+                "name": name,
+                "res_name": filename,
+                "res_model": res_model_string,
+                "res_id": id,
+                "mimetype": "application/pdf",
+                "type": "binary"
+            })
+            counter += 1
+            if counter % limit == 0:
                 env.cr.commit()
-                print(f"Bisher {start} Dateien angelegt.")
+                print(f"Bisher {counter} Dateien angelegt.")
         env.cr.commit()
-        print(f"Fertig! {start} Dateien angelegt.")
+        print(f"Fertig! {counter} Dateien angelegt.")
