03ce209663664063de431493258168700ec73f57
Tom-Has <t0m.has@pm.me>
2025-12-17 17:20:56 +0100

    Update read_record_values_to_csv.py

--- shell_scripts/read_record_values_to_csv.py
+++ shell_scripts/read_record_values_to_csv.py
@@ -2,23 +2,23 @@
 This snippet might be useful for custom modules where no view and/or export procedure is defined and an occasional list of given records is needed.
 """
 
-# adapt name as needed
-filename = 'export.csv'
+import csv
 
 # adapt to custom model as needed
-model = env['res.partner']
+model_name = 'my.custom.model'
+model = env[model_name]
 
 # adapt exportable fields as needed
-model_fields = ['id', 'name']
+model_fields = ['id', 'partner_id/id', 'partner_id/name']
+
+# adapt name as needed
+filename = model_name + '_export.csv'
 
 # adapt search filter as needed
 domain_filter = []
 
 # get records and write field values to file
-with open(filename, "w") as csv_file:
-    csv_file.write(model_fields.join(',') + '\n')
-    for rec in model.search(domain_filter):
-        """
-        this part needs to be updated to functioning logic
-        """
-        csv_file.write(f"" + "\n")
+with open(filename, "w", newline='', encoding='utf-8') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(model_fields)
+    writer.writerows(model.search(domain_filter).export_data(model_fields).get('datas', []))
